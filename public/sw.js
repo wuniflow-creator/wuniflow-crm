1d7bf86c360267962e7ccd034ba7e3f0bf1993ba
wuniflow-creator <wuniflow@gmail.com>
2026-09-25 03:48:13 -0300

    fix: preserve pending PWA navigation cache

--- public/sw.js
+++ public/sw.js
@@ -20,7 +20,11 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME && key !== NAV_CACHE_NAME)
+          .map((key) => caches.delete(key)),
+      ))
       .then(() => self.clients.claim()),
   );
 });
